76038b887ef9d0021301c68431eb9dc5dc34d2b7
kiresuah <kiresuah@gmail.com>
2016-07-23 21:41:15 -0400

    updated tests for result screen methods

--- src/components/resultscreen.js
+++ src/components/resultscreen.js
@@ -29,20 +29,13 @@ class ResultScreen extends Component {
 
 }
 
-export function parseCode(code) {
-  // check to make sure this is valid javascript - return error code if not
-  try {
-    eval(code);
-  } catch (err) {
-    return err.toString();
-  }
-  // find the start and stop point of all expressions
-  const expressionLocations = esprima.parse(code, { loc: true }).body
-                       .filter(node => node.type === 'ExpressionStatement')
-                       .map(node => ({ start: node.loc.start.line, end: node.loc.end.line }));
+export function getExpressions(code) {
+  return esprima.parse(code, { loc: true }).body
+                      .filter(node => node.type === 'ExpressionStatement');
+}
 
-  // clean the code into single line arguments to be iterated over
-  const cleaned = code.split('\n').reduce((program, line, index) => {
+export function cleanCode(code, expressionLocations) {
+  return code.split('\n').reduce((program, line, index) => {
     const compress = expressionLocations.some(({ start, end }) => (
       start !== end && index + 1 > start && index + 1 <= end
     ));
@@ -52,12 +45,21 @@ export function parseCode(code) {
     }
     return `${program}${compress ? '' : '\n'}${line}`;
   });
-  console.clear();
-  console.log(cleaned)
+}
+
+export function parseCode(code) {
+  // check to make sure this is valid javascript - return error code if not
+  try {
+    eval(code);
+  } catch (err) {
+    return err.toString();
+  }
+  // find the start and stop point of all expressions
+  const initialLocations = getExpressions(code).map(node => ({ start: node.loc.start.line, end: node.loc.end.line }));
+  // clean the code into single line arguments to be iterated over
+  const cleaned = cleanCode(code, initialLocations);
   // find all expressions in the cleaned code
-  const parsed = esprima.parse(cleaned, { loc: true }).body
-                .filter(node => node.type === 'ExpressionStatement')
-                .map(node => node.loc.start.line);
+  const finalLocations = getExpressions(cleaned).map(node => node.loc.start.line);
 
   // iterate over the cleaned code line by line, and execute the entire program
   // up to and including the current line, setting the result equal to the execution
@@ -65,9 +67,9 @@ export function parseCode(code) {
   let result = '';
   cleaned.split('\n').reduce((program, line, index) => {
     const newProgram = program + '\n' + line;
-    if (parsed.indexOf(index + 1) !== -1) {
+    if (finalLocations.indexOf(index + 1) !== -1) {
       try { // try/catch this just in case
-        result += eval(newProgram);
+        result += JSON.stringify(eval(newProgram));
       } catch (err) {
         console.log(err)
       }
--- test/components/resultscreen_test.js
+++ test/components/resultscreen_test.js
@@ -6,33 +6,115 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import sinon from 'sinon';
 
-import ResultScreen from '../../src/components/resultscreen';
+import ResultScreen, { getExpressions, cleanCode, parseCode }  from '../../src/components/resultscreen';
 import CodeMirror from 'react-codemirror';
 
+
 describe('Result Screen', () => {
-  let store;
-  let component;
-  const props = { height: 60 };
 
+  describe('Screen Behavior', () => {
+    let store;
+    let component;
+    const props = { height: 60 };
 
-  const mockStore = configureMockStore([thunk]);
-  const storeStateMock = {
-    displays: { code: 'test' },
-    options: { codemirror: { mode: 'javascript' } },
-  };
+    const mockStore = configureMockStore([thunk]);
+    const storeStateMock = {
+      displays: { code: 'test' },
+      options: { codemirror: { mode: 'javascript' } },
+    };
 
-  beforeEach(() => {
-    store = mockStore(storeStateMock);
-    component = shallow(<ResultScreen {...props} store={store} />).shallow();
+    beforeEach(() => {
+      store = mockStore(storeStateMock);
+      component = shallow(<ResultScreen {...props} store={store} />).shallow();
+    });
+
+    it('renders markup with a top level id of resultscreen', () => {
+      expect(component).to.exist;
+      expect(component.first().find('#resultscreen')).to.have.length(1);
+    });
+
+    it('contains a CodeMirror', () => {
+      expect(component.find(CodeMirror)).to.have.length(1);
+    });
+
   });
 
-  it('renders markup with a top level id of resultscreen', () => {
-    expect(component).to.exist;
-    expect(component.first().find('#resultscreen')).to.have.length(1);
+  describe('getExpressions', () => {
+    let code;
+    let expressions;
+    beforeEach(() => {
+      code = 'function go() {\n  return "Go!";\n}\n\nvar test = 1\ntest;\n\ngo();' +
+      '\n[1, 2, 3].map(num => {\n  return num * 2\n});';
+      expressions = getExpressions(code);
+    });
+
+    it('returns the correct number of expressions', () => {
+      expect(expressions.length).to.equal(3);
+    });
+
+    it('returns the correct line numbers for the test; expression', () => {
+      expect(expressions[0].loc.start.line).to.equal(6);
+      expect(expressions[0].loc.end.line).to.equal(6);
+    });
+
+    it('returns the correct line numbers for the go() function call expression', () => {
+      expect(expressions[1].loc.start.line).to.equal(8);
+      expect(expressions[1].loc.end.line).to.equal(8);
+    });
+
+    it('returns the correct line numbers for the multi-line map expression', () => {
+      expect(expressions[2].loc.start.line).to.equal(9);
+      expect(expressions[2].loc.end.line).to.equal(11);
+    });
   });
 
-  it('contains a CodeMirror', () => {
-    expect(component.find(CodeMirror)).to.have.length(1);
+  describe('cleanCode', () => {
+    const code = 'function go() {\n  return "Go!";\n}\n\nvar test = 1\ntest;\n\ngo();' +
+    '\n[1, 2, 3].map(num => {\n  return num * 2\n});';
+    const initalLocations = [{ start: 6, end: 6 }, { start: 8, end: 8 }, { start: 9, end: 11 }];
+    let clean;
+
+    beforeEach(() => {
+      clean = cleanCode(code, initalLocations);
+    });
+
+    it('returns the correct number of lines', () => {
+      expect(code.split('\n').length).to.equal(11);
+      expect(clean.split('\n').length).to.equal(9);
+    });
+
+    it('correctly adds a semicolon after num * 2', () => {
+      expect(clean.split('\n')[8].match(/;/g).length).to.equal(2);
+    });
+  });
+
+  describe('parseCode', () => {
+    const code = 'function go() {\n  return "Go!";\n}\n\nvar test = 1\ntest;\n\ngo();' +
+    '\n[1, 2, 3].map(num => {\n  return num * 2\n});\n1>0';
+    let result;
+    beforeEach(() => {
+      result = parseCode(code).split('\n');
+    });
+
+    it('returns the correct number of lines', () => {
+      expect(result.length).to.equal(11);
+    });
+
+    it('returns the correct response for statement test;', () => {
+      expect(result[5]).to.equal('1');
+    });
+
+    it('returns the correct response for function callgo()', () => {
+      expect(result[7]).to.equal('"Go!"');
+    });
+
+    it('returns the correct response for array operation map', () => {
+      expect(result[8]).to.equal('[2,4,6]');
+    });
+
+    it('returns the correct response for logical test1 > 0', () => {
+      expect(result[9]).to.equal('true');
+    });
   });
 
 });
